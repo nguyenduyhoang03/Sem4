9a8e5890cdeff51b6731d7ac339127cb61fbdfc3
nguyenduyhoang03 <hoangtop2003@gmail.com>
2025-02-27 09:37:54 +0700

    exam

---
+++ EAD EXAM/EXAM/src/main/java/com/example/exam/controller/StudentCoreController.java
@@ -0,0 +1,10 @@
+package com.example.exam.controller;
+
+import com.example.exam.service.StudentScoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class StudentCoreController {
+    private StudentScoreService studentScoreService;
+
+
+}
